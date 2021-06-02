30ac8d599d16d8f2164c1ee7832ced9496655c79
fatima <fatima@softylus.com>
2021-06-02 15:31:45 +0300

    models

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
+
+
+
 
 
 # Password validation
@@ -119,4 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = 'events.CustomUser'
+# AUTH_USER_MODEL = 'events.CustomUser'
--- events/models.py
+++ events/models.py
@@ -1,6 +1,73 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid # new
+# from django.contrib.auth.models import User
 
 
-class CustomUser(AbstractUser):
-    pass
+# class Events(models.Model):
+#     id = models.UUIDField(
+#         primary_key=True,
+#         # db_index=True,  # new
+#         default=uuid.uuid4,
+#         editable=False)
+#     Name = models.CharField(max_length=200)
+#     Photo = models.ImageField(upload_to='covers/', blank=True)
+#     Locations = models.CharField(max_length=200)
+#     start_date = models.DateField(null=True)
+#     end_date = models.DateField(null=True)
+#     user = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE,
+#         related_name='Events',
+#     )
+#
+#     def __str__(self):
+#         return self.Name
+
+# def get_absolute_url(self):
+#     return reverse('book_detail', args=[str(self.id)])
+
+
+class Assets(models.Model):
+        id = models.UUIDField(
+            primary_key=True,
+            # db_index=True,  # new
+            default=uuid.uuid4,
+            editable=False)
+        code = models.CharField(max_length=400)
+        featured_image = models.ImageField(upload_to='covers/', blank=True)
+        Latitude = models.CharField(max_length=200)
+        Google_maps_link = models.CharField(max_length=200)
+        Plus_code = models.CharField(max_length=200)
+        ASSETS_TYPE = [
+            ("IOS", 'IOS'),
+            ("ANDROID", 'Android'),
+        ]
+        ASSETS_TYPE = models.CharField(
+            max_length=8,
+            choices=ASSETS_TYPE,
+            default="IOS",
+        )
+
+        def __str__(self):
+            return self.code
+
+
+# class Locations(models.Model):
+#             id = models.UUIDField(
+#                 primary_key=True,
+#                 # db_index=True,  # new
+#                 default=uuid.uuid4,
+#                 editable=False)
+#             Name = models.CharField(max_length=200)
+#             Longitude = models.CharField(max_length=200)
+#             Latitude = models.CharField(max_length=200)
+#             Google_maps_link = models.CharField(max_length=200)
+#             Plus_code = models.CharField(max_length=200)
+#             assets = models.ForeignKey(
+#                 Assets,
+#                 on_delete=models.CASCADE,
+#                 related_name='Events',
+#             )
+#
+#             def __str__(self):
+#                 return self.Name
