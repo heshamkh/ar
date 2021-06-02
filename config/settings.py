655217d13dde2a3bec20a55dc6118840f805d0e7
fatima <fatima@softylus.com>
2021-06-02 16:19:02 +0300

    fix models

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'events',
 ]
 
 MIDDLEWARE = [
@@ -126,4 +125,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# AUTH_USER_MODEL = 'events.CustomUser'
--- events/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Assets
-
-admin.site.register(Assets)
