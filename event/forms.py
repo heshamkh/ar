645f1f69f88e7792865aec21fe603d0939bcae5e
fatima <fatima@softylus.com>
2021-06-09 13:02:22 +0300

    some design edits2

--- event/forms.py
+++ event/forms.py
@@ -71,3 +71,43 @@ class LocationCreationForm(forms.ModelForm):
         queryset=Asset.objects.all(),
         widget=forms.CheckboxSelectMultiple
     )
+    locations = forms.ModelMultipleChoiceField(queryset=Location.objects.all())
+
+    def __init__(self, *args, **kwargs):
+        # Only in case we build the form from an instance
+        # (otherwise, 'toppings' list should be empty)
+        if kwargs.get('instance'):
+            # We get the 'initial' keyword argument or initialize it
+            # as a dict if it didn't exist.
+            initial = kwargs.setdefault('initial', {})
+            # The widget for a ModelMultipleChoiceField expects
+            # a list of primary key for the selected data.
+            initial['locations'] = [t.pk for t in
+                                  kwargs['location'].location_set.all()]
+
+        forms.ModelForm.__init__(self, *args, **kwargs)
+
+    # Overriding save allows us to process the value of 'toppings' field
+    def save(self, commit=True):
+        # Get the unsaved Pizza instance
+        instance = forms.ModelForm.save(self, False)
+
+        # Prepare a 'save_m2m' method for the form,
+        old_save_m2m = self.save_m2m
+
+        def save_m2m():
+            old_save_m2m()
+            # This is where we actually link the pizza with toppings
+            instance.location_set.clear()
+            for location in self.cleaned_data['locations']:
+                instance.course_set.add(location)
+
+        self.save_m2m = save_m2m
+
+        # Do we need to save all changes now?
+        # Just like this
+        # if commit:
+        instance.save()
+        self.save_m2m()
+
+        return instance
