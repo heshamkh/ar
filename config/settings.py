a70501ac0b1106b7ca6e53325fc33d8b86192b5f
heshamkh <hesham@softylus.com>
2021-06-16 00:14:03 +0300

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -157,5 +157,5 @@ CRISPY_TEMPLATE_PACK = 'tailwind'
 LOGIN_REDIRECT_URL = 'events'
 LOGOUT_REDIRECT_URL = 'login'
 
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
