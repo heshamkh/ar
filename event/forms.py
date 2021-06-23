a3f562210287edf738cb355e07a64760547724e4
heshamkh <hesham@softylus.com>
2021-06-23 09:22:53 +0300

    tes

--- event/forms.py
+++ event/forms.py
@@ -57,7 +57,7 @@ class AssetCreationForm(forms.ModelForm):
             'Expiry_time': forms.TimeInput(format='%H:%M',attrs={'type': 'time'}),
         }
 
-    Asset_File = forms.FileField()
+    Asset_File = forms.FileField(null=True)
     Longitude = forms.NumberInput()
     Latitude = forms.NumberInput()
 
