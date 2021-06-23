0e1d88a4b8a3f1e05788ea1549b9e230f36a3647
heshamkh <hesham@softylus.com>
2021-06-23 15:59:26 +0300

    profile pic

--- event/models.py
+++ event/models.py
@@ -26,8 +26,8 @@ class Asset(models.Model):
     # featured_image = models.ImageField(upload_to='covers/', blank=True)
     # Google_maps_link = models.CharField(max_length=200)
     # ASSETS_TYPE = [("IOS", 'IOS'), ("ANDROID", 'Android'), ]
-    Longitude = models.DecimalField(null=True, max_digits=20, decimal_places=4)
-    Latitude = models.DecimalField(null=True, max_digits=20, decimal_places=4)
+    Longitude = models.DecimalField(null=True, max_digits=256, decimal_places=128)
+    Latitude = models.DecimalField(null=True, max_digits=265, decimal_places=128)
     # ASSETS_TYPE = models.CharField(
     #     max_length=8,
     #     choices=ASSETS_TYPE,
