cbd2a7085fe3917674e3e34dc675ec3927e55950
fatimaMarzouq <fatimaalqaryouti@hotmail.com>
2021-06-23 14:54:38 +0300

    phone number

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -8,11 +10,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('profilePic', 'phone',)
-
+        phone = PhoneNumberField(widget=PhoneNumberPrefixWidget(initial='IN'))
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         # fields = UserChangeForm.Meta.fields
         fields = ['username', 'first_name', 'last_name', 'email', 'profilePic', 'phone']
+        phone = PhoneNumberField(widget=PhoneNumberPrefixWidget(initial='IN'))
+
 
