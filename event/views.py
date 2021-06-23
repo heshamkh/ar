04c75691da858cf577cde80f22532bdd493a46e8
heshamkh <hesham@softylus.com>
2021-06-23 09:53:36 +0300

    tes

--- event/views.py
+++ event/views.py
@@ -32,14 +32,15 @@ class AssetListView(ListView):
 
 
 def asset_create(request):
-    form = AssetCreationForm(request.POST)
-    if form.is_valid():
-        # handle_uploaded_file(request.FILES['Asset_File'])
-        print(form.request)
-        form.save()
-        return redirect('/')
-    else:
-        print(form.errors)
+    if request.method == 'POST':
+        form = AssetCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            # handle_uploaded_file(request.FILES['Asset_File'])
+            print(form.request)
+            form.save()
+            return redirect('/')
+        else:
+            print(form.errors)
     context = {"form": form}
     return render(request, "Asset_new.html", context)
 
