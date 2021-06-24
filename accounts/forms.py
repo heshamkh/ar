8b1de64a9ec70920d4e639403f81ddd0679bc22c
fatimaMarzouq <fatimaalqaryouti@hotmail.com>
2021-06-24 14:52:44 +0300

    phone

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,8 +18,7 @@ class CustomUserChangeForm(UserChangeForm):
         model = CustomUser
         # fields = UserChangeForm.Meta.fields
         fields = ['username', 'first_name', 'last_name', 'email', 'profilePic', 'phone']
-        phone = PhoneNumberField(widget=PhoneNumberPrefixWidget(attrs={'placeholder': _(u'Cellphone'), 'class': "form-control"}),
-                                 label=_(u'Cellphone number'),
+        phone = PhoneNumberField(widget=PhoneNumberPrefixWidget(attrs={'class': "form-control"}),
                                  required=False,
                                  initial='+52')
 
